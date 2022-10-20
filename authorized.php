2f4f2bda4ca97af5d163973ddf36a73a4e2d3df1
Рома <sse1estt23@gmail.com>
2022-10-21 02:35:18 +0400

    create header

--- authorized.php
+++ authorized.php
@@ -8,8 +8,13 @@
  <html lang="en">
  <head>
  	<meta charset="UTF-8">
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
  	<link rel="stylesheet" href="css/style.css">
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
   <link 
   rel="stylesheet"
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
@@ -24,27 +29,37 @@
  <body>
   <div class="container">
     <header>
-      <div id="header_container">
-        <div class="hamburger-menu">
-          <input id="menu__toggle" type="checkbox" />
-          <label class="menu__btn" for="menu__toggle">
-            <span></span>
-          </label>
-          <ul class="menu__box">
-            <li><a class="menu__item" href="#">Мой профиль</a></li>
-            <li><a class="menu__item" href="#">Как это работает?</a></li>
-            <li><a class="menu__item" href="#">О нас</a></li>
-            <li><a class="menu__item" href="#">Статистика</a></li>
-            <li><a class="menu__item" href="#">Новости</a></li>
-          </ul>
+          <div id="navigation">
+          <div id="menu" onclick="onClickMenu()">
+            <div id="bar1" class="bar"></div>
+            <div id="bar2" class="bar"></div>
+            <div id="bar3" class="bar"></div>
+          </div>
+          
         </div>
-        <h1 class="content-header_title">
+        <div>
+          <h1 class="content-header_title">
           мойдодыр
-        </h1>
-      </div>
+          </h1>
+        </div>
+        <div class="logo">
+          <img src="css/logo.png" alt="(((">
+        </div>
+        <div class="profile_pic">
+          <img src="css/images.jpeg" alt=")))">
+        </div>
+      
+        
       
     </header>
-
+<ul class="nav" id="nav">
+            <li><a href="#">Мой профиль</a></li>
+            <li><a href="#">Как это работает?</a></li>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">Статистика</a></li>
+            <li><a href="#">Новости</a></li>
+          </ul>
+        
   </div>
  
 
@@ -54,5 +69,11 @@
  	<!-- <a href="../auth/logout.php">Выйти</a> -->
  
  </body>
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+ <script>
+  function onClickMenu() {
+    document.getElementById("nav").classList.toggle("change");
+
+  }
+ </script>
  </html>
