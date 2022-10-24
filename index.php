850d5165c3884f8401af61ac3e1f4d2b828250db
Рома <sse1estt23@gmail.com>
2022-10-24 08:19:37 +0400

    index page

--- index.php
+++ index.php
@@ -10,25 +10,39 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/style.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
 	<title>Система отслеживания качества</title>
 </head>
 <body>
-	<h2>Пожалуйста войдите в аккаунт!</h2>
-	<form action="auth/login.php" method="POST" class="edit">
-		<div class="form-group">
-			<input type="text" placeholder="Введите логин" name="login">
+	<div class="container">
+		<header class="indexHeader">
+			<div>
+				<h1 class="content-header_title">мойдодыр</h1>
+			</div>
+			<div class="logo">
+				<img src="css/logo.png" alt="(((">
+			</div>
+		</header>
+	<div class="indexMainPartContainer"> 
+		<h2 class="entrance">Вход</h2>
+		<div class="formGroupContainer">
+			<form action="auth/login.php" method="POST" class="edit">
+			<div class="form-group">
+				<input type="text" placeholder="Email" name="email">
+			</div>
+			<div class="form-group">
+				<input type="password" placeholder="Пароль" name = "password">
+			</div>
+			<button type="submit">ВОЙТИ</button><br>
+			<a id="linkToReg" href="reg.php">Нет аккаунта? Зарегистрироваться</a>
+			<?php  if($_SESSION['message']){
+				echo '<p class="msg"> ' . $_SESSION["message"] . ' </p>';
+			}
+			unset($_SESSION['message']);
+			?>
+		</form>
 		</div>
-		<div class="form-group">
-			<input type="password" placeholder="Введите пароль" name = "password">
-		</div>
-		<button type="submit" class = "btn btn-primary">Войти</button><br>
-		<a href="reg.php">Зарегестрироваться</a>
-		<?php  if($_SESSION['message']){
-			echo '<p class="msg"> ' . $_SESSION["message"] . ' </p>';
-		}
-		unset($_SESSION['message']);
-		?>
-	</form>
+		
+	</div>
 </body>
 </html>
